251bdab8052dd70fabdceb2b2ad48d0e278bd8d9
Victor Plushko <vicplushko33@gmail.com>
2020-04-04 04:07:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,7 +18,13 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash.each do |season, holiday|
+    if season == "summer" && holiday == "fourth_of_july"
+  end
+ end
+ holiday.values.last
 end
+
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
