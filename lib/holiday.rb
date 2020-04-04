bded558d78c5f5c7775ee4a73392fbfb0a29c4ca
Victor Plushko <vicplushko33@gmail.com>
2020-04-04 04:43:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
     holiday_hash.each do |season, holiday|
       puts "#{season.capitalize}"
-    holiday.each do |holiday, supply
+    holiday.each do |holiday, supply|
+      puts ""
 end
 
 def all_holidays_with_bbq(holiday_hash)
